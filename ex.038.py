e18f3a6e14b654293aa8a408decb3fe7c65ea05f
Matheus Silva <88094156+M4theus1@users.noreply.github.com>
2021-11-01 18:02:14 -0300

    Add files via upload

---
+++ ex.038.py
@@ -0,0 +1,9 @@
+n1 = int(input('Informe um número: '))
+n2 = int(input('Informe mais um número: '))
+
+if n1 > n2:
+    print('O primeiro é o maior!')
+elif n2 > n1:
+    print('O segundo é o maior!')
+else:
+    print('Os números são iguais!')
